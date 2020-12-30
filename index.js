d0cebcd4d16e23f1cba14892c293dea20abb4107
Richard Girges <richardgirges@gmail.com>
2020-12-30 12:56:59 -0800

    allow html and text mode

--- index.js
+++ index.js
@@ -14,13 +14,18 @@ app.get("/truth", (req, res) => {
   request(
     {
       uri:
-        "https://feedback.wizards.com/forums/918667-mtg-arena-bugs-product-suggestions/suggestions/41537692-shuffler-algoritm?page=" + randomPage,
+        "https://feedback.wizards.com/forums/918667-mtg-arena-bugs-product-suggestions/suggestions/41537692-shuffler-algoritm?page=" +
+        randomPage,
     },
     function (error, response, body) {
       const comments = [];
       const $ = cheerio.load(body);
       $(".uvListItem .uvUserActionBody .typeset").each((index, el) => {
-        comments.push($(el).text());
+        if (req.query.mode === "text") {
+          comments.push($(el).text());
+        } else {
+          comments.push($(el).html());
+        }
       });
 
       res.send(comments[Math.floor(Math.random() * comments.length)]);
