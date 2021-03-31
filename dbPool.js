6971b08cff23af36f33e1999e4cee9a8daf14c52
Richard Girges <richardgirges@gmail.com>
2021-03-31 12:17:54 -0700

    configure pg ssl for production

--- dbPool.js
+++ dbPool.js
@@ -1,5 +1,8 @@
 const { Pool } = require("pg");
-const dbPool = new Pool({ connectionString: process.env.DATABASE_URL });
+const dbPool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production",
+});
 
 dbPool.on("error", (err, client) => {
   console.error("Unexpected error on idle client", err);
